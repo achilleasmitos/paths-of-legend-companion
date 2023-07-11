54c10dc5affe20ccb348cd98107baf842daefc28
Achilleas Mitos <axmito@gmail.com>
2023-07-11 17:55:31 +0300

    Laying the foundations -Started work on the actual app - Fixed build warnings - Fixed deployment to gh-pages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders correctly", () => {
+    const view = render(<App />);
+    expect(view.container).toBeInTheDocument();
+  });
+
+  test("renders header and sub-header", () => {
+    render(<App />);
+    const header1 = screen.getAllByRole("heading")[0];
+    const header2 = screen.getAllByRole("heading")[1];
+    expect(header1).toBeInTheDocument();
+    expect(header2).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Paths of Legend</h1>
+        <h2>Companion</h2>
       </header>
+      <p>
+        Welcome to the companion site for Paths of Legend, an in-development RPG
+        where your choices matter!
+      </p>
     </div>
   );
 }
