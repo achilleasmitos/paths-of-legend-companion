c1b20212ea66f84b7e27ab84588190b318f33ada
Achilleas Mitos <axmito@gmail.com>
2023-07-16 20:45:57 +0300

    Initialized React Router - Added npm package - Created routes for the basic elements - Created start pages for those elements - Added buttons and links to navigate back and forth

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
@@ -7,10 +9,21 @@ function App() {
         <h1>Paths of Legend</h1>
         <h2>Companion</h2>
       </header>
-      <p>
+      <p className="sub-title">
         Welcome to the companion site for Paths of Legend, an in-development RPG
         where your choices matter!
       </p>
+      <ul>
+        <li>
+          <Link to={"wiki"}>Wiki</Link>
+        </li>
+        <li>
+          <Link to={"battle-maker"}>Battle maker</Link>
+        </li>
+        <li>
+          <Link to={"character-creation"}>Character creation</Link>
+        </li>
+      </ul>
     </div>
   );
 }
